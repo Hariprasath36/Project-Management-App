fee5f53c667ce27e0239332a2a446154ca41a920
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-09-28 18:17:52 +0530

    added projectControllers,routs and create project

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,6 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 /* ROUTE IMPORTS */
+const projectRouts_1 = __importDefault(require("./routes/projectRouts"));
 /*CONFGURATION*/
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -24,6 +25,7 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("This is home route");
 });
+app.use("/projects", projectRouts_1.default);
 /*SERVER*/
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
