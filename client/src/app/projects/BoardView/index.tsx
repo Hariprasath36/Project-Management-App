d5e0ede1b87d1af190dea53b0763efaf231224eb
HARIPRASATH R <111111073+Hariprasath36@users.noreply.github.com>
2024-10-02 21:14:54 +0530

    need to continue in 3:28:22

---
+++ client/src/app/projects/BoardView/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type BoardProps = {
+    id: string;
+    setIsModalNewTaskOpen:
+}
+
+const BoardView = (props: BoardProps) => {
+  return <div>BoardView</div>
+};
+
+export default BoardView;
